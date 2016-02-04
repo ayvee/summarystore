c58b5e80f1ea9e2734c1a5815c10447532a94135
Ashish Vulimiri <a.vulimiri@samsung.com>
2016-02-04 13:38:34 -0800

    fixed various bugs

--- code/TimeDecayedStore/src/main/java/com/samsung/sra/DataStore/FixedSizeBucketMerger.java
+++ code/TimeDecayedStore/src/main/java/com/samsung/sra/DataStore/FixedSizeBucketMerger.java
@@ -3,15 +3,45 @@ package com.samsung.sra.DataStore;
 import java.util.*;
 
 public class FixedSizeBucketMerger implements BucketMerger {
-    private final int bucketSize;
+    private final int B;
 
     public FixedSizeBucketMerger(int bucketSize) {
-        this.bucketSize = bucketSize;
+        this.B = bucketSize;
+    }
+
+    private static int bucketSize(BucketInfo bucketInfo) {
+        return bucketInfo.endN - bucketInfo.startN + 1;
     }
 
     public List<List<BucketID>> merge(LinkedHashMap<BucketID, BucketInfo> baseBuckets, int N0, int N) {
         List<List<BucketID>> ret = new ArrayList<List<BucketID>>();
-        int bucketNum = -1, numBuckets = baseBuckets.size();
+        List<BucketID> mergeList = null;
+        BucketInfo prevBucketInfo = null;
+        for (BucketInfo bucketInfo: baseBuckets.values()) {
+            assert 1 <= bucketSize(bucketInfo) && bucketSize(bucketInfo) <= B;
+            if (bucketSize(bucketInfo) == B) {
+                // bucket is already full, do nothing
+                assert prevBucketInfo == null;
+                ret.add(Collections.singletonList(bucketInfo.bucketID));
+            } else { // bucket isn't full, size is < B
+                if (prevBucketInfo == null) {
+                    prevBucketInfo = bucketInfo;
+                    mergeList = new ArrayList<BucketID>();
+                    mergeList.add(bucketInfo.bucketID);
+                    ret.add(mergeList);
+                    continue;
+                } else {
+                    assert bucketSize(prevBucketInfo) + bucketSize(bucketInfo) <= B; // actually size(bucketInfo) should be 1
+                    assert prevBucketInfo.endN == bucketInfo.startN + 1;
+                    prevBucketInfo.endN = bucketInfo.endN;
+                    mergeList.add(bucketInfo.bucketID);
+                    if (bucketSize(prevBucketInfo) == B) { // bucket is now full, seal it
+                        prevBucketInfo = null;
+                    }
+                }
+            }
+        }
+        /*int bucketNum = -1, numBuckets = baseBuckets.size();
         for (Iterator<Map.Entry<BucketID, BucketInfo>> iter = baseBuckets.entrySet().iterator(); iter.hasNext(); ) {
             bucketNum += 1;
             Map.Entry<BucketID, BucketInfo> entry = iter.next();
@@ -19,9 +49,9 @@ public class FixedSizeBucketMerger implements BucketMerger {
             BucketInfo bucketInfo = entry.getValue();
             ArrayList<BucketID> mergeList = new ArrayList<BucketID>();
             mergeList.add(bucketID);
-            if (bucketInfo.endN == bucketInfo.startN && bucketNum <= numBuckets - bucketSize) {
+            if (bucketInfo.endN == bucketInfo.startN && bucketNum <= numBuckets - B) {
                 //System.out.println("Merge target " + bucketID);
-                for (int i = 0; i < bucketSize - 1; ++i) {
+                for (int i = 0; i < B - 1; ++i) {
                     bucketNum += 1;
                     BucketInfo mergee = iter.next().getValue();
                     //System.out.println("Merge source " + mergee.bucketID);
@@ -30,7 +60,7 @@ public class FixedSizeBucketMerger implements BucketMerger {
                 }
             }
             ret.add(mergeList);
-        }
+        }*/
         return ret;
     }
 }
