6e87769e7128310779611fb6f9096dfc798e4fa2
Ashish Vulimiri <a.vulimiri@samsung.com>
2016-05-09 14:51:39 -0700

    removed old experiment scripts

--- code/SummaryStore/run.sh
+++ code/SummaryStore/run.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
+if [ $# -lt 1 ]
+then
+	echo "SYNTAX: $0 className [classArgs]"
+	exit 2
+fi
 cp=".:target/SummaryStore-1.0-SNAPSHOT.jar"
 for jar in target/lib/*
 do
 	cp="$cp:$jar"
 done
-#java -cp $cp com.samsung.sra.DataStore.SummaryStore
 
-#java -cp $cp com.samsung.sra.DataStoreExperiments.AgeLengthSampler $*
-#java -cp $cp com.samsung.sra.DataStoreExperiments.CompareWindowingSchemes
-#java -Xmx15G -cp $cp com.samsung.sra.DataStoreExperiments.MeasureThroughput
-#java -Xmx15G -cp $cp com.samsung.sra.DataStoreExperiments.PopulateData $*
-java -Xmx15G -cp $cp com.samsung.sra.DataStoreExperiments.QueryOnDiskStore $*
-#java -cp $cp com.samsung.sra.DataStoreExperiments.VaryN | tee varyN.tsv
-#java -cp $cp com.samsung.sra.DataStoreExperiments.VaryQueries | tee varyQueries.tsv
+className=$1
+shift
+java -Xmx15G -cp $cp com.samsung.sra.DataStoreExperiments.$className $*
 #java -cp $cp com.samsung.sra.DataStoreExperiments.AgeLengthEffect > age-length-matrix.tsv 2> age-length-scatter.tsv
 #java -cp $cp com.samsung.sra.WindowingOptimizer.UpperBoundOptimizer 2> opt.log | tee opt.tsv
-#java -cp $cp com.samsung.sra.WindowingOptimizer.StorageVsAccuracy
-#java -cp $cp com.samsung.sra.WindowingOptimizer.AgeLengthVsAccuracy
 
 #./plot.sh
 #gnuplot age-length.gp
