4c9715e8bb4a6a647ef82ba9b1ca52ee0043afc3
Ashish Vulimiri <a.vulimiri@samsung.com>
2016-05-31 16:26:49 -0700

    modified experiment code to work on one store at a time

--- code/SummaryStore/src/main/java/com/samsung/sra/DataStoreExperiments/CompareWindowingSchemes.java
+++ code/SummaryStore/src/main/java/com/samsung/sra/DataStoreExperiments/CompareWindowingSchemes.java
@@ -9,13 +9,13 @@ import org.slf4j.LoggerFactory;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.Serializable;
 import java.nio.file.*;
 import java.util.*;
 import java.util.function.ToDoubleFunction;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 class CompareWindowingSchemes {
     private static final int numAgeLengthClasses = 8;
@@ -33,22 +33,21 @@ class CompareWindowingSchemes {
     private static final FSTConfiguration fstConf;
     static {
         fstConf = FSTConfiguration.createDefaultConfiguration();
-        fstConf.registerClass(Statistics.class);
+        fstConf.registerClass(StoreStats.class);
     }
 
-    private static Map<String, SummaryStore> discoverStores(String directory, long N) throws IOException, RocksDBException {
+    /** Returns (decay function name) -> (store location prefix) mapping for all stores in the directory
+     * with the specified N */
+    private static Map<String, String> discoverStores(String directory, long N) throws IOException, RocksDBException {
         Pattern pattern = Pattern.compile("(.*N" + N + "\\.D([^\\.]+))\\.bucketStore.*");
-        Map<String, SummaryStore> stores = new LinkedHashMap<>();
+        Map<String, String> stores = new LinkedHashMap<>();
         try (DirectoryStream<Path> paths = Files.newDirectoryStream(Paths.get(directory), "N" + N + ".*.bucketStore")) {
             for (Path path: paths) {
                 if (Files.isDirectory(path)) {
                     Matcher matcher = pattern.matcher(path.toString());
                     if (matcher.matches()) {
                         String prefix = matcher.group(1), decay = matcher.group(2);
-                        // WARNING: setting cache size to N, i.e. load everything off RocksDB into memory
-                        SummaryStore store = new SummaryStore(prefix, N);
-                        stores.put(decay, store);
-                        store.warmupCache();
+                        stores.put(decay, prefix);
                     }
                 }
             }
@@ -56,54 +55,91 @@ class CompareWindowingSchemes {
         return stores;
     }
 
+    private static class Query {
+        final long l, r;
+        final QueryType type;
+        final Object[] params;
+
+        Query(long l, long r, QueryType type, Object[] params) {
+            this.l = l;
+            this.r = r;
+            this.type = type;
+            this.params = params;
+        }
+    }
+
+    private static class StoreStats implements Serializable {
+        final long sizeInBytes;
+        final LinkedHashMap<AgeLengthClass, Statistics> queryStats;
+
+        StoreStats(long sizeInBytes, Collection<AgeLengthClass> alClasses) {
+            this.sizeInBytes = sizeInBytes;
+            queryStats = new LinkedHashMap<>();
+            alClasses.forEach(alClass -> queryStats.put(alClass, new Statistics(true)));
+        }
+    }
+
     /**
-     * Compute stats for each (decay function, age length class) pair. Results will be memoized if the argument is true
+     * Compute stats for each store. Results will be memoized to disk if the argument is true
      */
-    private static LinkedHashMap<String, LinkedHashMap<AgeLengthClass, Statistics>> runExperiment(
+    private static LinkedHashMap<String, StoreStats> runExperiment(
             String directory, long N, boolean memoize) throws Exception {
-        LinkedHashMap<String, LinkedHashMap<AgeLengthClass, Statistics>> results;
         String memoFile = memoize ? String.format("%s/N%d.profile", directory, N) : null;
         if (memoize) {
             try {
                 byte[] serialized = Files.readAllBytes(Paths.get(memoFile));
-                return (LinkedHashMap<String, LinkedHashMap<AgeLengthClass, Statistics>>)fstConf.asObject(serialized);
+                return (LinkedHashMap<String, StoreStats>)fstConf.asObject(serialized);
             } catch (NoSuchFileException e) {
                 logger.info("memoized results not found, running experiment");
             } catch (IOException | ClassCastException e) {
                 logger.warn("failed to deserialize memoized results", e);
             }
         }
-        Map<String, SummaryStore> stores = discoverStores(directory, N);
+
+        Map<String, String> stores = discoverStores(directory, N);
         if (stores.isEmpty()) {
             throw new IllegalArgumentException("no stores found with N = " + N);
         }
-        results = new LinkedHashMap<>();
-
-        //stores.forEach((name, store) -> System.out.println(name + " = " + store.getStoreSizeInBytes() + " bytes"));
-
         List<AgeLengthClass> alClasses = AgeLengthSampler.getAgeLengthClasses(N, N, numAgeLengthClasses, numAgeLengthClasses);
 
-        stores.keySet().forEach(name -> results.put(name, new LinkedHashMap<>()));
+        Map<AgeLengthClass, List<Query>> workload = new LinkedHashMap<>();
         Random random = new Random();
-        alClasses.forEach(alClass -> {
-            logger.info("Processing age length class {}", alClass);
-            stores.keySet().forEach(name -> results.get(name).put(alClass, new Statistics(true)));
-            IntStream.range(0, numRandomQueriesPerClass).parallel().forEach(q -> {
+        for (AgeLengthClass alClass: alClasses) {
+            List<Query> queries = new ArrayList<>();
+            for (int q = 0; q < numRandomQueriesPerClass; ++q) {
                 Pair<Long> ageLength = alClass.sample(random);
                 long age = ageLength.first(), length = ageLength.second();
                 long l = N - length + 1 - age, r = N - age;
-                if (l < 0 || r >= N) return;
-                double trueCount = r - l + 1;
-                stores.forEach((name, store) -> {
+                if (0 <= l && r < N) {
+                    queries.add(new Query(l, r, QueryType.COUNT, null));
+                }
+            }
+            workload.put(alClass, queries);
+        }
+
+        LinkedHashMap<String, StoreStats> results = new LinkedHashMap<>();
+        for (Map.Entry<String, String> entry: stores.entrySet()) {
+            String decay = entry.getKey();
+            // WARNING: setting cache size to N, i.e. loading all data into main memory
+            SummaryStore store = new SummaryStore(entry.getValue(), N);
+            store.warmupCache();
+
+            StoreStats storeStats = new StoreStats(store.getStoreSizeInBytes(), alClasses);
+            for (AgeLengthClass alClass: alClasses) {
+                logger.info("Processing {}{}", decay, alClass);
+                Statistics stats = storeStats.queryStats.get(alClass);
+                workload.get(alClass).parallelStream().forEach(q -> {
                     try {
-                        double estCount = (long)store.query(streamID, l, r, QueryType.COUNT, null);
-                        results.get(name).get(alClass).addObservation(estCount / trueCount - 1);
-                    } catch (Exception e) { // java streams don't like exceptions, apparently
-                        e.printStackTrace();
+                        double trueCount = q.r - q.l + 1;
+                        double estCount = (long) store.query(streamID, q.l, q.r, q.type, q.params);
+                        stats.addObservation(estCount / trueCount - 1);
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
                     }
                 });
-            });
-        });
+            }
+            results.put(decay, storeStats);
+        }
 
         // FIXME: close() stores when done
 
@@ -132,13 +168,13 @@ class CompareWindowingSchemes {
             return;
         }
 
-        LinkedHashMap<String, LinkedHashMap<AgeLengthClass, Statistics>> results = runExperiment(directory, N, true);
+        LinkedHashMap<String, StoreStats> results = runExperiment(directory, N, true);
 
         System.out.println("cost of");
         LinkedHashMap<String, Double> decayFunctionCosts = new LinkedHashMap<>(); // compute cost of each decay function
-        results.forEach((decayFunction, perClassResults) -> {
-            Collection<Statistics> eachClassStatistics = perClassResults.values();
-            Collection<Double> eachClassWeight = perClassResults.keySet().stream().
+        results.forEach((decayFunction, stats) -> {
+            Collection<Statistics> eachClassStatistics = stats.queryStats.values();
+            Collection<Double> eachClassWeight = stats.queryStats.keySet().stream().
                     mapToDouble(weightFunction).
                     boxed().collect(Collectors.toList());
             // construct the aggregate weighted mixture distribution over all the classes
