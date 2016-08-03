d8ce9f6a8124b688853a6b614a835063227c7305
Ashish Vulimiri <a.vulimiri@samsung.com>
2016-08-03 10:53:31 -0700

    generalized count estimator to work on arbitrary intervals

--- code/SummaryStore/src/main/java/com/samsung/sra/DataStore/Aggregates/SimpleCountOperator.java
+++ code/SummaryStore/src/main/java/com/samsung/sra/DataStore/Aggregates/SimpleCountOperator.java
@@ -1,6 +1,8 @@
 package com.samsung.sra.DataStore.Aggregates;
 
 import com.samsung.sra.DataStore.*;
+import org.apache.commons.lang.mutable.MutableDouble;
+import org.apache.commons.lang.mutable.MutableLong;
 import org.apache.commons.math3.util.Pair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -75,8 +77,10 @@ public class SimpleCountOperator implements WindowOperator<Long, Long, Double, P
         private long tml = -1; // start timestamp of second bucket
         private long tmr = -1; // start timestamp of last bucket (if there is > 1 bucket)
         private long te = -1; // end timestamp of last bucket
-        private long Cl = -1, Cm = -1, Cr = -1; // count of first bucket, all middle buckets, and last bucket
-        private double cv_t = 0; // sigma_t / mu_t
+        private long Cl = -1; // count of first bucket
+        private long Cm = -1; // count of all middle buckets (if there are > 2 buckets)
+        private long Cr = -1; // count of last bucket (if there is > 1 bucket)
+        private double sigma_t = 0, mu_t = 0; // mean and variance of interarrival time
 
         Estimator(double numSDs) {
             this.numSDs = numSDs;
@@ -86,32 +90,65 @@ public class SimpleCountOperator implements WindowOperator<Long, Long, Double, P
             this(2);
         }
 
-        public ResultError<Double, Pair<Double, Double>> estimate(long t0, long t1) {
-            assert ts <= t0 && (tml == -1 || t0 <= tml) && (tmr == -1 || tmr <= t1) && (t1 <= te);
-            double ans, sd;
-            double Ctot; // total count, i.e. right endpoint of 100% CI
-            logger.debug("timestamps = [{}, {}, {}, {}], counts = {}, {}, {}", ts, tml, tmr, te, Cl, Cm, Cr);
-            if (tmr == -1) { // only one bucket
-                double t = t1 - t0 + 1;
-                double T = te - ts + 1;
-                logger.debug("t = {}, T = {}", t, T);
-                ans = Cl * t/T;
-                sd = cv_t * Math.sqrt((Cl * t/T) * t/T * (1 - t/T));
-                Ctot = Cl;
-            } else {
-                double tl = tml-1 - t0 + 1;
-                double Tl = tml-1 - ts + 1;
-                double tr = t1 - tmr + 1;
-                double Tr = te - tmr + 1;
-                logger.debug("tl = {}, Tl = {}, tr = {}, Tr = {}", tl, Tl, tr, Tr);
-                ans = Cl * tl/Tl + Cm + Cr * tr/Tr;
-                sd = cv_t * Math.sqrt(
-                        (Cl * tl/Tl) * tl/Tl * (1 - tl/Tl) +
-                        (Cr * tr/Tr) * tr/Tr * (1 - tr/Tr));
-                Ctot = Cl + Cm + Cr;
+        /** What is the length of [l, r]? */
+        private static long length(long l, long r) {
+            return r - l + 1;
+        }
+
+        /** How much do [a, b] and [p, q] overlap? */
+        private static long overlap(long a, long b, long p, long q) {
+            return Math.max(Math.min(q, b) - Math.max(a, p) + 1, 0);
+        }
+
+        /**
+         * Given count[T0, T1] = C, update mean and 1/cv_t * variance for
+         * count(portion of[t0, t1] that intersects [T0, T1])
+         */
+        private static void conditionalEstimate(MutableDouble mean, MutableDouble var,
+                                                long C, long T0, long T1, long t0, long t1) {
+            long overlap = overlap(T0, T1, t0, t1);
+            if (C != -1 && overlap != 0) {
+                logger.debug("conditional estimate: C = {}, [T0, T1] = [{}, {}], [t0, t1] = [{}, {}], overlap = {}",
+                        C, T0, T1, t0, t1, overlap);
+                double ratio = (double)overlap / length(T0, T1);
+                mean.add(C * ratio);
+                var.add(C * ratio * (1 - ratio));
             }
-            double CIl = Math.max(0d, ans - numSDs * sd);
-            double CIr = Math.min(Ctot, ans + numSDs * sd);
+        }
+
+        private static void unconditionalEstimate(MutableDouble mean, MutableDouble var, long overlap, double mu_t) {
+            assert overlap >= 0;
+            if (overlap > 0) {
+                logger.debug("unconditional estimate: overlap = {}", overlap);
+                mean.add(overlap / mu_t);
+                var.add(overlap / mu_t);
+            }
+        }
+
+        public ResultError<Double, Pair<Double, Double>> estimate(long t0, long t1) {
+            // Check overlap with each of these intervals:
+            //     (-inf, ts-1], [ts, tml-1], [tml, tmr-1], [tmr, te], [te+1, inf)
+            // Middle three intervals: we know counts, do a conditional estimate (proportional count)
+            // First and last intervals: do an unconditional estimate (T / mu)
+            logger.debug("timestamps = [{}, {}, {}, {}], counts = ({}, {}, {})", ts, tml, tmr, te, Cl, Cm, Cr);
+            MutableDouble mean = new MutableDouble(0), var = new MutableDouble(0);
+            // FIXME: should we use C/T instead of long-term average mu_t for unconditional?
+            unconditionalEstimate(mean, var, overlap(t0, t1, Long.MIN_VALUE, ts-1), mu_t);
+            conditionalEstimate(mean, var, Cl, ts, tml-1, t0, t1);
+            conditionalEstimate(mean, var, Cm, tml, tmr-1, t0, t1);
+            conditionalEstimate(mean, var, Cr, tmr, te, t0, t1);
+            unconditionalEstimate(mean, var, overlap(t0, t1, te+1, Long.MAX_VALUE), mu_t);
+            double ans = mean.toDouble(), sd = sigma_t / mu_t * Math.sqrt(var.toDouble());
+            double CIl = Math.max(ans - numSDs * sd, 0d), CIr = ans + numSDs * sd;
+            if (ts <= t0) {
+                if (t1 <= tml) {
+                    CIr = Math.min(CIr, Cl);
+                } else if (Cm != -1 && t1 <= tmr) {
+                    CIr = Math.min(CIr, Cl+Cm);
+                } else if (t1 <= te) {
+                    CIr = Math.max(CIr, Cl+Cm+Cr);
+                }
+            }
             return new ResultError<>(ans, new Pair<>(CIl, CIr));
         }
     }
@@ -119,13 +156,14 @@ public class SimpleCountOperator implements WindowOperator<Long, Long, Double, P
     @Override
     public ResultError<Double, Pair<Double, Double>> query(StreamStatistics streamStats, long T0, long T1, Stream<Bucket> buckets, Function<Bucket, Long> countRetriever, long t0, long t1, Object... params) {
         Estimator estimator = new Estimator();
-        estimator.ts = T0;
-        estimator.te = T1;
+        MutableLong numBuckets = new MutableLong(0L); // not a plain long because of Java Stream limitations
         buckets.forEach(b -> {
-            if (estimator.Cl == -1) { // first bucket
-                assert estimator.ts == b.tStart;
+            numBuckets.increment();
+            if (numBuckets.toLong() == 1) { // first bucket
+                assert b.tStart == T0;
+                estimator.ts = T0;
                 estimator.Cl = countRetriever.apply(b);
-            } else if (estimator.Cm == -1) { // second bucket
+            } else if (numBuckets.toLong() == 2) { // second bucket
                 estimator.tml = b.tStart;
                 estimator.Cm = estimator.Cr = countRetriever.apply(b);
                 estimator.tmr = b.tStart;
@@ -135,13 +173,22 @@ public class SimpleCountOperator implements WindowOperator<Long, Long, Double, P
                 estimator.Cm += estimator.Cr;
             }
         });
-        if (estimator.tml == estimator.tmr) { // there were <= 2 buckets, i.e. there were no "middle" buckets
-            estimator.Cm = 0;
+        assert numBuckets.toLong() > 0;
+        if (numBuckets.toLong() == 1) { // no middle or right buckets
+            estimator.tml = T1+1;
+            estimator.te = T1;
+            estimator.Cm = -1;
+            estimator.Cr = -1;
+        } else if (numBuckets.toLong() == 2) { // no middle bucket
+            estimator.te = T1;
+            estimator.Cm = -1;
         } else {
+            estimator.te = T1;
             // the loop above set estimator.Cm = count of all buckets starting from 2nd including last; subtract last now
             estimator.Cm -= estimator.Cr;
         }
-        estimator.cv_t = streamStats.getCVInterarrival();
+        estimator.mu_t = streamStats.getMeanInterarrival();
+        estimator.sigma_t = streamStats.getSDInterarrival();
         return estimator.estimate(t0, t1);
     }
 
