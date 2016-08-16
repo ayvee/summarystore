17c7365493aa9afc933b7fb9e956cb94d4a7cfd8
Ashish Vulimiri <a.vulimiri@samsung.com>
2016-08-16 14:44:49 -0700

    toml experiment config parser (not yet plugged into experiment code)

--- code/SummaryStore/src/main/java/com/samsung/sra/DataStoreExperiments/UniformValues.java
+++ code/SummaryStore/src/main/java/com/samsung/sra/DataStoreExperiments/UniformValues.java
@@ -3,16 +3,16 @@ package com.samsung.sra.DataStoreExperiments;
 import java.util.Random;
 
 public class UniformValues implements ValueDistribution {
-    private final long l, r;
+    private final long min, max;
 
-    public UniformValues(long l, long r) {
-        this.l = l;
-        this.r = r;
-        assert l <= r;
+    public UniformValues(long min, long max) {
+        this.min = min;
+        this.max = max;
+        assert min <= max;
     }
 
     @Override
     public long next(Random random) {
-        return l + Math.abs(random.nextLong()) % (r - l + 1);
+        return min + Math.abs(random.nextLong()) % (max - min + 1);
     }
 }
