9b05daca6dca8bcb3d0a326df38f81acc1d5c4dc
Ashish Vulimiri <a.vulimiri@samsung.com>
2016-10-04 10:32:10 -0700

    option to clear kernel page cache before each run

---
+++ code/SummaryStore/scripts/drop-caches.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [[ ! -e /proc/sys/vm/drop_caches ]]
+then
+    echo "ERROR: Linux only"
+    exit 1
+fi
+if [[ $EUID -ne 0 ]]
+then
+    echo "ERROR: must be run as root"
+    exit 1
+fi
+sync
+echo 3 > /proc/sys/vm/drop_caches
