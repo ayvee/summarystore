95df9751417a05317705932a49c9aa659b9efee1
Ashish Vulimiri <a.vulimiri@samsung.com>
2016-10-20 15:41:22 -0700

    error trend, TSM stub code (WIP)

---
+++ code/SummaryStore/src/main/java/com/samsung/sra/DataStoreExperiments/TSMWorkloadGenerator.java
@@ -0,0 +1,8 @@
+package com.samsung.sra.DataStoreExperiments;
+
+public class TSMWorkloadGenerator implements WorkloadGenerator {
+    @Override
+    public Workload generate(long T0, long T1) {
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+}
