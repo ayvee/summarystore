39763f2d752540daa6d909fae6382e0380991c9e
Ashish Vulimiri <a.vulimiri@samsung.com>
2017-08-15 17:12:32 -0700

    batched ingest (with stream unloading) in throughput tester

--- SummaryStore/src/main/java/com/samsung/sra/experiments/MeasureThroughput.java
+++ SummaryStore/src/main/java/com/samsung/sra/experiments/MeasureThroughput.java
@@ -1,13 +1,14 @@
 package com.samsung.sra.experiments;
 
+import com.samsung.sra.datastore.RationalPowerWindowing;
+import com.samsung.sra.datastore.SummaryStore;
 import com.samsung.sra.datastore.aggregates.CMSOperator;
 import com.samsung.sra.datastore.aggregates.SimpleCountOperator;
 import com.samsung.sra.datastore.ingest.CountBasedWBMH;
-import com.samsung.sra.datastore.RationalPowerWindowing;
-import com.samsung.sra.datastore.SummaryStore;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class MeasureThroughput {
@@ -15,19 +16,22 @@ public class MeasureThroughput {
     private static final Logger logger = LoggerFactory.getLogger(MeasureThroughput.class);
 
     public static void main(String[] args) throws Exception {
-        if (args.length != 2) {
-            System.err.println("SYNTAX: MeasureThroughput numValuesPerThread numThreads");
+        if (args.length <= 2) {
+            System.err.println("SYNTAX: MeasureThroughput numValuesPerThread numThreads [numParallelThreads]");
             System.exit(2);
         }
         long T = Long.parseLong(args[0].replace("_", ""));
         int nThreads = Integer.parseInt(args[1]);
+        Semaphore parallelismSem = args.length > 2
+                ? new Semaphore(Integer.parseInt(args[2]))
+                : null;
         Runtime.getRuntime().exec(new String[]{"sh", "-c", "rm -rf " + directory}).waitFor();
 
         try (SummaryStore store = new SummaryStore(directory, new SummaryStore.Options().setKeepReadIndexes(false))) {
             StreamWriter[] writers = new StreamWriter[nThreads];
             Thread[] writerThreads = new Thread[nThreads];
             for (int i = 0; i < nThreads; ++i) {
-                writers[i] = new StreamWriter(store, i, T);
+                writers[i] = new StreamWriter(store, parallelismSem, i, T);
                 writerThreads[i] = new Thread(writers[i], i + "-appender");
             }
             long w0 = System.currentTimeMillis();
@@ -51,11 +55,13 @@ public class MeasureThroughput {
     private static class StreamWriter implements Runnable {
         private final long streamID, N;
         private final SummaryStore store;
+        private final Semaphore semaphore;
         private final CountBasedWBMH wbmh;
         private final ThreadLocalRandom random;
 
-        private StreamWriter(SummaryStore store, long streamID, long N) throws Exception {
+        private StreamWriter(SummaryStore store, Semaphore semaphore, long streamID, long N) throws Exception {
             this.store = store;
+            this.semaphore = semaphore;
             this.streamID = streamID;
             this.N = N;
             this.random = ThreadLocalRandom.current();
@@ -70,6 +76,7 @@ public class MeasureThroughput {
 
         @Override
         public void run() {
+            if (semaphore != null) semaphore.acquireUninterruptibly();
             try {
                 for (long t = 0; t < N; ++t) {
                     long v = random.nextLong(100);
@@ -79,8 +86,14 @@ public class MeasureThroughput {
                 wbmh.setBufferSize(0);*/
                 wbmh.flushAndSetUnbuffered();
                 logger.info("Populated stream {}", streamID);
+                if (semaphore != null) {
+                    store.unloadStream(streamID);
+                    logger.info("Unloaded stream {}", streamID);
+                }
             } catch (Exception e) {
                 throw new RuntimeException(e);
+            } finally {
+                if (semaphore != null) semaphore.release();
             }
         }
     }
