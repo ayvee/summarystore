e8b978e1cd552d432321f15156f5528da430f361
Ashish Vulimiri <a.vulimiri@samsung.com>
2018-12-10 10:43:42 -0800

    remove unused scripts

--- SummaryStore/consts.sh
+++
@@ -1,5 +0,0 @@
-if [ -z $XMX ]
-then
-    XMX=10G
-fi
-#>&2 echo "[`date`] Heap-size $XMX"
--- SummaryStore/eval/scripts/drop-caches.sh
+++ SummaryStore/eval/scripts/drop-caches.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-if [[ ! -e /proc/sys/vm/drop_caches ]]
-then
+# TODO: move into src/main/resources?
+if [[ ! -e /proc/sys/vm/drop_caches ]]; then
     echo "ERROR: Linux only"
     exit 1
 fi
-if [[ $EUID -ne 0 ]]
-then
+if [[ $EUID -ne 0 ]]; then
     echo "ERROR: must be run as root"
     exit 1
 fi
